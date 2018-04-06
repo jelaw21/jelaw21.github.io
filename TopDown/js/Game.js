86fb997582f0cc61def650654d4a01f126a61075
jelaw21 <jelaw21@gmail.com>
2018-04-06 10:49:45 -0400

    built the top-down map

--- TopDown/js/Game.js
+++ TopDown/js/Game.js
@@ -8,8 +8,18 @@ TopDown.Game.prototype  = {
 
         this.map = this.game.add.tilemap('level1');
 
+        //first parameter is name in Tiled
+        this.map.addTilesetImage('tiles', 'gameTiles');
 
+        //create layer
+        this.backgroundLayer = this.map.createLayer('backgroundLayer');
+        this.blockedLayer = this.map.createLayer('blockedLayer');
+
+        //collision on blockedLayer
+        this.map.setCollisionBetween(1, 2000, true, 'blockedLayer');
+
+        this.backgroundLayer.resizeWorld();
 
     }
 
-}
+};
