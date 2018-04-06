dcf1393b042840bd0467c652a3708183e9549f79
jelaw21 <jelaw21@gmail.com>
2018-04-06 10:16:30 -0400

    changed TopDownGame to TopDown in main.js

--- TopDown/js/main.js
+++ TopDown/js/main.js
@@ -1,9 +1,9 @@
-var TopDownGame = TopDownGame || {};
+var TopDown = TopDown || {};
 
-TopDownGame.game = new Phaser.Game(160, 160, Phaser.AUTO, '');
+TopDown.game = new Phaser.Game(160, 160, Phaser.AUTO, '');
 
-TopDownGame.game.state.add('Boot', TopDownGame.Boot);
-TopDownGame.game.state.add('Preload', TopDownGame.Preload);
-TopDownGame.game.state.add('Game', TopDownGame.Game);
+TopDown.game.state.add('Boot', TopDownGame.Boot);
+TopDown.game.state.add('Preload', TopDownGame.Preload);
+TopDown.game.state.add('Game', TopDownGame.Game);
 
-TopDownGame.game.state.start('Boot');
+TopDown.game.state.start('Boot');
