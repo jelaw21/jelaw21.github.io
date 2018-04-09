3afc40078c5d8f9ba97df0498fc0db6926d2117f
jelaw21 <jelaw21@gmail.com>
2018-04-09 11:07:29 -0400

    Debug 9: further tweaking loading screen

--- ConceptTest/js/Preload.js
+++ ConceptTest/js/Preload.js
@@ -9,7 +9,6 @@ Test.Preload.prototype = {
         this.splash.scale.setTo(0.3, 0.3);
         this.splash.anchor.setTo(0.5);
         this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 120, 'preloadbar');
-        this.preloadBar.scale.setTo(0.8,1.0);
         this.preloadBar.anchor.setTo(0.5);
 
         this.load.setPreloadSprite(this.preloadBar);
