e7f302fce76d3bbac79845b2adee1a77775e3019
jelaw21 <jelaw21@gmail.com>
2018-04-09 10:45:42 -0400

    Debug 7: setScale to just affect the y

--- ConceptTest/js/Preload.js
+++ ConceptTest/js/Preload.js
@@ -9,7 +9,7 @@ Test.Preload.prototype = {
         this.splash.scale.setTo(0.20, 0.20);
         this.splash.anchor.setTo(0.5);
         this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY + 120, 'preloadbar');
-        this.preloadBar.scale.setTo(0.2,0.2);
+        this.preloadBar.scale.setTo(1.0,0.2);
         this.preloadBar.anchor.setTo(0.5);
 
         this.load.setPreloadSprite(this.preloadBar);
