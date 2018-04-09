2175f2870e859813e5940373f01d66f52a38875f
jelaw21 <jelaw21@gmail.com>
2018-04-09 15:22:45 -0400

    finished create forest need to add coin images

--- ConceptTest/js/Level1.js
+++ ConceptTest/js/Level1.js
@@ -33,7 +33,7 @@ Test.Level1.prototype = {
         this.grass.resizeWorld();
 
         this.player = this.game.add.sprite(300, 600, 'playerS');
-        console.log(JSON.stringify(items[0]));
+        console.log(JSON.stringify(this.items[0]));
         this.player.anchor.set(0.5, 0.5);
 
         this.walk = this.player.animations.add('walk');
