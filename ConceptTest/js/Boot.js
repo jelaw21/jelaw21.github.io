282ca996f5d98c838fa754fea5abaaddf036bfc6
jelaw21 <jelaw21@gmail.com>
2018-04-09 09:49:01 -0400

    created files completed Boot and Main

---
+++ ConceptTest/js/Boot.js
@@ -0,0 +1,26 @@
+var Test = Test || {};
+
+Test.Boot = function(){};
+
+Test.Boot.prototype = {
+
+    preload: function(){
+        this.load.image('logo', 'assets.images/GWK.png');
+        this.load.image('preloadbar', 'assets/images/smoke.jpg');
+
+    },
+
+    create: function(){
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.minWidth = 512;
+        this.scale.minHeight = 384;
+        this.scale.maxWidth = 1280;
+        this.scale.maxHeigt = 640;
+
+        this.scale.pageAlignHorizontally = true;
+        this.scale.setScreenSize(true);
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.state.start('Preload');
+    }
+
+}
---
+++ ConceptTest/js/main.js
@@ -0,0 +1,11 @@
+var Test = ConceptTest || {};
+
+Test.game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO, "");
+
+Test.game.state.add('Boot', Test.Boot);
+Test.game.state.add('Preload', Test.Preload);
+Test.game.state.add('MainMenu', Test.MainMenu);
+Test.game.state.add('Level1', Test.Level1);
+Test.game.state.add('Level2', Test.Level2);
+
+Test.game.state.start('Boot');
