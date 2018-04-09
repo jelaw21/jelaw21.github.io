ddbc83d6a72d1204737ee7263e0002c5bce12d6d
jelaw21 <jelaw21@gmail.com>
2018-04-09 12:49:02 -0400

    created the first layer to forest

--- ConceptTest/js/Level1.js
+++ ConceptTest/js/Level1.js
@@ -7,10 +7,12 @@ Test.Level1.prototype = {
     create: function(){
 
         this.map = this.add.tilemap('forest');
-        this.map.addTilesetImage('backgroundTiles1');
         this.map.addTilesetImage('backgroundTiles2');
+        this.map.addTilesetImage('backgroundTiles1');
+
 
         this.grass = this.map.createLayer('grassLayer');
+        this.groundCover = this.map.createLayer('groundCover');
 
 
 
