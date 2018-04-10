bcc01dbc3ea8946b1de7cd36db1f1323f82822a6
jelaw21 <jelaw21@gmail.com>
2018-04-10 13:31:32 -0400

    Phaser3 Tut

--- Smithy/js/main.js
+++ Smithy/js/main.js
@@ -30,7 +30,7 @@ function preload() {
 };
 
 function create(){
-    var background = this.add.image(0, 0, 'background').setOrigin(0,0).setScale(3);
+    var background = this.add.image(0, 0, 'background').setScale(3);
     var inset = this.add.image(0, 0, 'inset').setScale(3);
     Phaser.Display.Align.In.Center(background, this.add.zone(400,300, 800, 600));
     Phaser.Display.Align.In.Center(inset, background)
