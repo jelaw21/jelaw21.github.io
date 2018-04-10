0954bafa0f84b90f693afa0199968b15917ffa0f
jelaw21 <jelaw21@gmail.com>
2018-04-10 13:51:07 -0400

    Phaser3 Tut

--- Smithy/js/main.js
+++ Smithy/js/main.js
@@ -34,6 +34,12 @@ function create(){
     var inset = this.add.image(0, 0, 'inset').setScale(5);
     Phaser.Display.Align.In.Center(background, this.add.zone(game.config.width/2, game.config.height/2, game.config.width, game.config.height));
     Phaser.Display.Align.In.Center(inset, background)
+    var hitBoxEdge = this.load.image(0,0,'hitspotBack').setScale(5);
+    var hitBox = this.load.image(0, 0, 'hitspotFore');
+    Phaser.Display.Align.In.Center(hitBoxEdge, background);
+    Phaser.Display.Align.In.Center(hitBox, background);
+
+
 };
 function update(){
 
