577523030cea8e3b0190b653b72ad44f215ad2bb
jelaw21 <jelaw21@gmail.com>
2018-04-10 12:08:46 -0400

    Phaser3 Tut

---
+++ Smithy/js/main.js
@@ -0,0 +1,39 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {y:300},
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('background', 'assets/PNG/panel_brown');
+    this.load.image('inset', 'assets/PNG/panelInset)beigeLight.png');
+    this.load.image('barLend', 'assets/PNG/barBack_horizontalLeft.png');
+    this.load.image('barRend', 'assets/PNG/barBack_horizontalRight.png');
+    this.load.image('barMid', 'assets/PNG/barBack_horizontalMid.png');
+    this.load.image('hitspotBack', 'assets/PNG/iconCircle_beige.png');
+    this.load.image('hitspotFore', 'assets/PNG/iconCircle_blue.png');
+    this.load.image('anvil', 'assets/Anvil2.png');
+};
+
+function create(){
+    this.add.image(400, 300, 'background');
+    this.add.image(400, 300, 'inset');
+
+};
+function update(){
+
+};
